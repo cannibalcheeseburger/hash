da112c8de4698d1ac6b05c5ad3a5928284336a90
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-02-26 00:09:35 +0530

    abhi kuch nhi h

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void read_input(){
+  #ifndef ONLINE_JUDGE
+  freopen("a.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+}
+
+int main(){
+    read_input();
+    int D,I,S,V,F,B,E,L;
+    string name;
+    cin>>D>>I>>S>>V>>F;
+    int temps = S,tempv=V;
+    while(temps--){
+        
+    }
+    while(tempv--){
+        
+    }
+    return 0;
+}

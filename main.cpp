9db37fa8373ac001ad43c0a059d523e6aa2db7cf
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-02-26 02:00:08 +0530

    simul

--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-using namespace std;
+
 
 void read_input(){
   #ifndef ONLINE_JUDGE
   freopen("a.txt","r",stdin);
   freopen("output.txt","w",stdout);
   #endif
+}
+
+
+void simulation(int I,map<string,vector<int>> &streets){
+    vector<vector<pair<string,int>>> intersections(I);
+    
+
+}
+
+void print_answer(vector<pair<pair<int,int>,vector<pair<string,int>>>> &answer){
+    cout<<answer.size();//intersections
+        for(auto itr = answer.begin();itr!=answer.end();itr++){
+            cout<<itr->first.first<<"\n";
+            cout<<itr->first.second<<"\n";
+            for(int i =0;i<itr->first.second;i++){
+                cout<<itr->second[i].first<<" ";
+                cout<<itr->second[i].second<<"\n";
+            }
+        }
+}
+
+
+vector<int> time_calc(map<string,vector<int>> &streets, vector<pair<int,vector<string>>> &cars){
+    vector <int> timetaken(cars.size());
+
+    for(int i=0;i<cars.size();i++){
+        int temp=(cars[i].first-1);
+        vector <string> path=cars[i].second;
+        for (int i=1;i<path.size();i++){
+            temp+=streets[path[i]][2];
+        }
+        timetaken[i]=temp;
+    }
+    return timetaken;
 }
 
 int main(){
@@ -17,7 +51,7 @@ int main(){
     int temps = S,tempv=V;
     map<string,vector<int>> streets;
     vector<pair<int,vector<string>>> cars;
-    
+    vector<pair<pair<int,int>,vector<pair<string,int>>>> answer;
     while(temps--){//input streets
         int B,E,L;
         string name;
@@ -43,6 +77,14 @@ int main(){
         cars.push_back(make_pair(P,names));
     }
     
+
+    auto timetaken = time_calc(streets,cars);
+    simulation(I);
+    print_answer(answer);
+    return 0;
+}
+
+
 /**     checking output remove befor submission
     for (auto itr = streets.begin(); itr != streets.end(); ++itr) {  
         cout<<itr->first;
@@ -53,8 +95,6 @@ int main(){
         cout<<"\n";
     } 
    
-
-
     cout<<"ok input";
 
     for (auto itr = cars.begin(); itr != cars.end(); ++itr) {  
@@ -65,8 +105,5 @@ int main(){
         }
         cout<<"\n";
     } 
+
 **/
-
-
-    return 0;
-}
